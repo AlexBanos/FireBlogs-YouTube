77fbc79fe072e0a5f22d2a78c6b069fb481ecd79
Alexandros Banos <alexpatekala@gmail.com>
2023-07-12 00:34:47 +0200

    Setup of home page blogs and hamburger menu timestamp 1:17

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <template>
   <div class="app-wrapper">
     <div class="app">
+      <Navigation />
       <router-view />
+      <Footer />
     </div>
   </div>
 </template>
 
 <script>
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
 export default {
   name: "app",
-  components: {},
+  components: { Navigation, Footer },
   data() {
     return {};
   },
-  created() {},
-  mounted() {},
+  created() { },
+  mounted() { },
   methods: {},
   watch: {},
 };
